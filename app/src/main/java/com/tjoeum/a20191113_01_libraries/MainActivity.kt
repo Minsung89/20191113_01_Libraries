1dc50544145538569fa7e03701f4553fa16bbe39
Minsung89 <minsung0217@gmail.com>
2019-11-13 19:51:11 +0900

    Glide를 히용한 이미지 예제

--- app/src/main/java/com/tjoeum/a20191113_01_libraries/MainActivity.kt
+++ app/src/main/java/com/tjoeum/a20191113_01_libraries/MainActivity.kt
@@ -2,6 +2,8 @@ package com.tjoeum.a20191113_01_libraries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -9,12 +11,18 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
     }
 
     override fun setValues() {
+
+        Glide.with(this).load("http://image.kyobobook.co.kr/newimages/giftshop_new/goods/400/1539/S1458632370750.jpg").into(profileImg)
+
     }
 
 }
